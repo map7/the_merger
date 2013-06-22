d4901782f5b803feb611a5975868f2bf8ebe098a
Michael Pope <map7@corsairsolutions.com.au>
2013-06-22 15:45:37 +1000

    Read a variable from a yaml file

--- lib/the_merger.rb
+++ lib/the_merger.rb
@@ -1,12 +1,20 @@
 require "the_merger/version"
 
 module TheMerger
+  #
+  # Default settings for TheMerger
+  # 
+  # merge_model:   User
+  #
+  
   def self.merger
     puts 'Mail Merge test'
   end
   
   def self.merge_fields(original_body, *fields)
-
+    self.parse_config
+    puts @merge_model
+    
     User.all.each do |user|
       puts "User: #{user.firstname}"
       
@@ -19,5 +27,16 @@ module TheMerger
     end
   end
 
-  
+  private
+
+  def self.parse_config
+    path = "#{Rails.root}/config/the_merger.yml"
+    
+    if File.exists?(path)
+      conf=YAML::load(IO.read(path))
+      @merge_model = conf["merge_model"]
+    else
+      @merge_model = "User"
+    end
+  end  
 end
