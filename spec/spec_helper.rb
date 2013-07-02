41515b8c2cdedd66c21dee32df22281996ebdedd
Michael Pope <map7@corsairsolutions.com.au>
2013-07-02 23:00:22 +1000

    Creating a spec_helper file with the requirements for Combustion

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,12 @@
-require 'the_merger'
+require 'rubygems'
+require 'bundler/setup'
+
+require 'combustion'
+
+Combustion.initialize! :action_view, :action_mailer
+
+require 'rspec/rails'
+
+# RSpec.configure do |config|
+#   config.use_transactional_fixtures = true
+# end
