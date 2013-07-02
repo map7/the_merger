5a7b279497956ed9a43bb2af12d194d13cc6b176
Michael Pope <map7@corsairsolutions.com.au>
2013-07-02 23:26:59 +1000

    Add two test tables

--- spec/internal/db/schema.rb
+++ spec/internal/db/schema.rb
@@ -1,3 +1,23 @@
 ActiveRecord::Schema.define do
-  #
+  create_table "letters", :force => true do |t|
+    t.string   "subject"
+    t.string   "body"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "email"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.string   "mobile"
+    t.string   "home_phone"
+    t.string   "work_phone"
+    t.string   "address"
+    t.string   "suburb"
+    t.string   "state"
+    t.string   "postcode"
+  end
 end
