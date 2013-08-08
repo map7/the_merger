0757b839942ace98678e5fbd90b73b74c590071c
Michael Pope <map7@corsairsolutions.com.au>
2013-08-08 22:18:03 +1000

    Allow HTML format to be used in the_merger

--- app/mailer/the_merger/mailer.rb
+++ app/mailer/the_merger/mailer.rb
@@ -3,7 +3,9 @@ class TheMerger::Mailer < ActionMailer::Base
   def batch_mail(from,subject,body,person)
     mail(from: from, 
          to: person.email,
-         subject: subject,
-         body: body)
+         subject: subject) do |format|
+      format.html {body.html_safe}
+      format.text {body}      
+    end
   end  
 end
